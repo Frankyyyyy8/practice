9a8ec6f33fc1b947e71cc14881a5c71403312db2
frank <farruxvloging@gmail.com>
2026-04-17 02:50:34 +0900

    fix: learn keyword & defaults arguments

--- funtion.py
+++ funtion.py
@@ -5,7 +5,7 @@
 (4) Scope
 '''
 
-print("====== DEFINE vs CALL =====")
+print("====== DEFINE (parameter) vs CALL (argument) =====")
 # build in function > print() type()
 # Function - reusable block of code!
 # Instead of block {} in JAVA, Python uses indentation!
@@ -28,3 +28,19 @@ print("result1:", result1)
 
 result2 = greeting("Justin")
 print("result2:", result2)
+
+print("====== Keyword & default arguments =====")
+
+
+# DEFINE
+def give_greet(name, age=22):
+    print("give_greet is executed")
+    return f"Hi {name}, you are {age} years old!"
+
+
+# CALL
+result3 = give_greet(name="Justin", age=28)
+print("result3:", result3)
+
+result4 = give_greet("John")
+print("result4:", result4)
