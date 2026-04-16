b2eccba7ddacff2728a5b201545c78e4e7c224a9
frank <farruxvloging@gmail.com>
2026-04-17 01:18:02 +0900

    feat: python system variables & special methods via Dunder

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ print("result:", result)
 
 ''' In Python, there are builtin tools:
 (1) TYPES > int float str list dict
-(2) FUNCTION > print(), len(), input(), type()
+(2) FUNCTION > print(), len(), input(), type(), str(), int()
 (3) CONSTANTS > True, False, None
 '''
 
