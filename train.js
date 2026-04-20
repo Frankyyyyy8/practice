056c900608bf5289a3f9267ca1f542cd0b073572
frank <farruxvloging@gmail.com>
2026-04-20 11:59:54 +0900

    feat: B task

--- train.js
+++ train.js
@@ -1,3 +1,27 @@
+/* B-TASK
+ Shunday function tuzing,
+ u 1ta string parametrga ega bolsin, 
+ hamda osha stringda qatnashgan raqamlarni 
+ sonini bizga return qilsin.
+
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+*/
+
+// Masalani yechimi:
+function countDigits(dig) {
+  let count = 0;
+
+  for (let ele of dig) {
+    if (ele >= "0" && ele <= "9") {
+      count++;
+    }
+  }
+
+  return count;
+}
+const result = countDigits("ad2a54y79wet0sfgb9");
+console.log("result:", result);
+
 /* A-TASK: 
 SAVOL: Shunday 2 parametrli function tuzing, 
  hamda birinchi parametrdagi letterni ikkinchi parametrdagi 
@@ -7,18 +31,18 @@ MASALAN: countLetter("e", "engineer") 3ni return qiladi.
 
 // Masalani yechimi:
 
-function countLetter(a, b) {
-  let c = b.split("");
-  let count = 0;
+// function countLetter(a, b) {
+//   let c = b.split("");
+//   let count = 0;
 
-  for (let i = 0; i <= c.length; i++) {
-    if (c[i] === "e") {
-      count += 1;
-    }
-  }
+//   for (let i = 0; i <= c.length; i++) {
+//     if (c[i] === "e") {
+//       count += 1;
+//     }
+//   }
 
-  return count;
-}
+//   return count;
+// }
 
-const result = countLetter("e", "engineer");
-console.log("result:", result);
+// const result = countLetter("e", "engineer");
+// console.log("result:", result);
