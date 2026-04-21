51a4a076d9ed34c5ff051b6375bf1f103c10bd88
frank <farruxvloging@gmail.com>
2026-04-22 03:25:28 +0900

    feat: Polimorhism concept

--- class.ip.py
+++ class.ip.py
@@ -31,8 +31,8 @@ class Dog(Animal):  # child
     def introduce(self):
         print(f"{self.name} says: {self.sound}-{self.sound}")
 
-    def protect(self):
-        print("Yes, I can protect you!")
+    def make_voice(self):
+        print(f"the {self.name} says {self.sound}")
 
 
 class Cat(Animal):  # child
@@ -88,3 +88,23 @@ print(Dog.description)
 print(dog.voice, fish.voice)
 print("dog.status:", dog._status)
 print("cat.status:", cat._status)
+
+
+print("===== Polymorhism =====")
+
+dog.make_voice()
+fish.make_voice()
+
+print("-------")
+# fish > Fish > Animal > object
+a = isinstance(fish, Fish)
+b = isinstance(fish, Animal)
+c = isinstance(fish, object)
+d = isinstance("MIT", object)
+result = a and b and c and d
+print(f"the result:  {result}")
+
+# Fish > Animal > object
+data1 = issubclass(Fish, Animal)
+data2 = issubclass(Animal, object)
+print("data:", data1, data2)
