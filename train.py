d7cbbe72daee2be1a7940764f527ee4551b4b95f
frank <farruxvloging@gmail.com>
2026-05-06 19:03:17 +0900

    feat: I-TASK homework

--- train.py
+++ train.py
@@ -1,4 +1,28 @@
 '''
+I-TASK (PYTHON)
+
+Shunday function tuzing, unga string argument pass bolsin. 
+Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: get_digits("m14i1t") return qiladi "141"
+'''
+
+# Masalani yechimi:
+
+
+def get_digits(str):
+    result = ""
+
+    for numb in str:
+        if numb.isdigit():
+            result += numb
+
+    return result
+
+
+result = get_digits("m14i1t")
+print("result:", result)
+
+'''
 G-TASK (PYTHON)
 
  Shunday function tuzingki unga integerlardan iborat
@@ -10,20 +34,20 @@ MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 # Masalani yechimi:
 
 
-def get_highest_index(arr):
-    if not arr:
-        return -1
+# def get_highest_index(arr):
+#     if not arr:
+#         return -1
 
-    max_value = arr[0]
-    max_index = 0
+#     max_value = arr[0]
+#     max_index = 0
 
-    for i, a in enumerate(arr):
-        if a > max_value:
-            max_value = a
-            max_index = i
+#     for i, a in enumerate(arr):
+#         if a > max_value:
+#             max_value = a
+#             max_index = i
 
-    return max_index
+#     return max_index
 
 
-result = get_highest_index([5, 21, 12, 21, 8])
-print("result:", result)
+# result = get_highest_index([5, 21, 12, 21, 8])
+# print("result:", result)
